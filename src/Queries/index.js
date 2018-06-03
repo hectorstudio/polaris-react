fbb5ae28ad2a600f64dc89d61de4720224f94bf4
Ben <ben@benclarke.me>
2018-06-03 17:56:51 +0100

    Fetch Library Query

--- src/Queries/index.js
+++ src/Queries/index.js
@@ -7,3 +7,14 @@ export const FETCH_LIBRARIES = gql`
         }
     }
 `
+
+export const FETCH_LIBRARY = gql`
+    query Library($name: String, $type: FeedType!, $offset: Int, $limit: Int){
+        library(name: $name) {
+            type,
+            feed( type: $type, offset: $offset, limit: $limit ) {
+                title
+            }
+        }
+    }
+`
