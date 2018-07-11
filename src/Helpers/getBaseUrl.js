7267cd6131f7ac59866fbfbddb5b59e17fa20fba
Ben Clarke <ben.clarke@LFLMacBookB.local>
2018-07-11 11:02:12 +0100

    Added Basebath generation

--- src/Helpers/getBaseUrl.js
+++ src/Helpers/getBaseUrl.js
@@ -1,5 +1,15 @@
 const getBaseUrl = () => {
     let url = 'http://atalanta.bysh.me:8080';
+    
+    // eslint-disable-next-line
+    let path;
+
+    // For use when we have it running on servers
+    if (typeof window !== 'undefined') {
+        path = window.location.protocol + '//' + window.location.host;
+    } else {
+        console.log('Cant Find Base Url');
+    }
 
     return url
 }
