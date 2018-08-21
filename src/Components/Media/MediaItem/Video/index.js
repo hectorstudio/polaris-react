ff019d236a98b1652dbc79d21599ee0a177899ab
Ben Clarke <ben.clarke@LFLMacBookB.local>
2018-08-21 15:51:32 +0100

    Chromecase

--- src/Components/Media/MediaItem/Video/index.js
+++ src/Components/Media/MediaItem/Video/index.js
@@ -1,26 +1,29 @@
 import React from 'react'
 import videojs from 'video.js'
+import chromecast from '@silvermine/videojs-chromecast'
+
+videojs.registerPlugin('chromecast', chromecast);
 
 export default class Video extends React.Component {
-  componentDidMount() {
-    this.player = videojs(this.videoNode, this.props, function onPlayerReady() {
-
-    });
-  }
-
-  componentWillUnmount() {
-    if (this.player) {
-      this.player.dispose()
+    componentDidMount() {
+        this.player = videojs(this.videoNode, this.props, function onPlayerReady() {
+            
+        });
     }
-  }
 
-  render() {
-    return (
-      <div>
-        <div data-vjs-player>
-          <video ref={node => this.videoNode = node} className="video-js"></video>
-        </div>
-      </div>
-    )
-  }
+    componentWillUnmount() {
+        if (this.player) {
+            this.player.dispose()
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <div data-vjs-player>
+                    <video ref={node => this.videoNode = node} className="video-js"></video>
+                </div>
+            </div>
+        )
+    }
 }
