a05cdfb4205154928d6d4c512b6ed61f0c61f53b
Ben Clarke <ben.clarke@LFLMacBookB.local>
2018-11-21 08:26:42 +0000

    Fix fetchPolicy

--- src/Containers/Dashboard/RenderUpNext.jsx
+++ src/Containers/Dashboard/RenderUpNext.jsx
@@ -13,8 +13,7 @@ import { MediaCardWrap } from './Styles';
 const RenderUpNext = () => (
   <Query
     query={UP_NEXT}
-    pollInterval={5000}
-    no-cache
+    fetchPolicy="no-cache"
   >
 
     {({ loading, error, data }) => {
